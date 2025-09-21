418998c5a47d3a0c78c0a6287e6ee603bf143a8a
Jonkimi <JonkimyLiao@gmail.com>
2025-09-21 11:09:23 -0400

    feat(plugins): add nvim-autopairs
    Adds the `windwp/nvim-autopairs` plugin to provide automatic closing of
    brackets, parentheses, and quotes. This enhances the editing experience
    by automating pair insertion.
    
    The plugin is configured to load on insert mode activation for better
    startup performance.

--- lua/user/plugins/tool.lua
+++ lua/user/plugins/tool.lua
@@ -9,4 +9,23 @@ tool["jghauser/follow-md-links.nvim"] = {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
 
+-- tool["altermo/ultimate-autopair.nvim"] = {
+--     event={'InsertEnter','CmdlineEnter'},
+--     branch='v0.6', --recommended as each new version will have breaking changes
+--     opts={
+--         --Config goes here
+--     },
+-- }
+
+tool["windwp/nvim-autopairs"] = {
+    event = "InsertEnter",
+    config = true,
+    -- opts = {
+    --     check_ts = true,
+    --     -- disable_filetype = { "TelescopePrompt" },
+    -- },
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
+
 return tool
