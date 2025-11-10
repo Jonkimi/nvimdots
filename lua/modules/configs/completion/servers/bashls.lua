0ecc54c37cc4a110bb325a4e3cdbfda3e27e4f91
Jonkimi <JonkimyLiao@gmail.com>
2025-11-10 09:16:19 -0400

    fix(bashls): remove zsh from filetypes due to lack of support

--- lua/modules/configs/completion/servers/bashls.lua
+++ lua/modules/configs/completion/servers/bashls.lua
@@ -1,5 +1,6 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/bashls.lua
 return {
 	cmd = { "bash-language-server", "start" },
-	filetypes = { "bash", "sh", "zsh" },
+	-- zsh not supported https://github.com/bash-lsp/bash-language-server/issues/252
+	filetypes = { "bash", "sh"},
 }
