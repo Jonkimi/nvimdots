bcc02c4e3ea748b63a7741cef27b781207620c3d
Jonkimi <JonkimyLiao@gmail.com>
2025-12-19 03:08:04 -0400

    feat(ui): add bufferline ordinal numbers and scope.nvim

---
+++ lua/user/configs/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+	-- 关闭 buffer 显示，开始 标签 数字标记
+	options = {
+		-- mode = "tabs",
+		numbers = "ordinal"
+	}
+}
---
+++ lua/user/plugins/ui.lua
@@ -0,0 +1,8 @@
+local ui = {}
+
+ui["tiagovla/scope.nvim"] = {
+	event = "VeryLazy",
+	config = true,
+}
+
+return ui
