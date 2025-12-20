7d1e97104932d97f0969b03119c02c48f8f53571
Jonkimi <JonkimyLiao@gmail.com>
2025-12-20 01:36:27 -0400

    feat(keymap): add treesitter, flash, and hop keymaps
    remove NvimTree which-key group and keymaps

---
+++ lua/user/configs/which-key.lua
@@ -0,0 +1,6 @@
+-- local cmp_icon = require("modules.utils.icons").get("cmp", true),
+return {
+	spec = {
+		{ "<leader>n", group = require("modules.utils.icons").get("cmp", true).treesitter .. "Treesitter Node" },
+	},
+}
--- lua/user/keymap/tool.lua
+++ lua/user/keymap/tool.lua
@@ -1,14 +1,11 @@
+local bind = require("keymap.bind")
+local map_callback = bind.map_callback
+
+-- delete nvim tree keymap
+vim.api.nvim_del_keymap('n', '<leader>nr')
 
 local mappings = {
 	-- Custom: Save cursor position before visual selection and restore on <Esc>
-	["n|v"] = {
-		cmd = "mav",
-		options = {
-			noremap = true,
-			silent = true,
-			desc = "edit: Start Visual Mode and Mark",
-		},
-	},
 	["n|<leader>bs"] = {
 		cmd = "<Cmd>Telescope scope buffers<CR>",
 		options = {
@@ -16,8 +13,39 @@ local mappings = {
 			silent = true,
 			desc = "tool: Find Scope Buffers",
 		},
-	}
-
+	},
+	["n|<leader>nn"] = {
+		cmd = "<Cmd>HopNodes<CR>",
+		options = {
+			noremap = true,
+			silent = true,
+			desc = "Hopes Node Jump",
+		},
+	},
+	["n|<leader>nv"] = {
+		cmd = "",
+		options = {
+			callback = function()
+				vim.cmd("normal! ma")  -- 跳转到标记 a 的精确位置
+				require("flash").treesitter()
+			end,
+			noremap = true,
+			silent = true,
+			desc = "Flash Treesitter Visual selection",
+		},
+	},
+	["n|<leader>nf"] = {
+		cmd = "",
+		options = {
+			callback = function()
+				vim.cmd("normal! ma")  -- 跳转到标记 a 的精确位置
+				require("flash").treesitter_search()
+			end,
+			noremap = true,
+			silent = true,
+			desc = "Flash Treesitter Find",
+		},
+	},
 }
 
 return mappings
