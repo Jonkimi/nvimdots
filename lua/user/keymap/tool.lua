bdf1886b72dba920a512992255647164b9518e4b
Jonkimi <JonkimyLiao@gmail.com>
2025-12-20 22:37:09 -0400

    feat(flash): integrate flash.nvim for enhanced visual mode and jumps

--- lua/user/keymap/tool.lua
+++ lua/user/keymap/tool.lua
@@ -2,7 +2,7 @@ local bind = require("keymap.bind")
 local map_callback = bind.map_callback
 
 -- delete nvim tree keymap
-vim.api.nvim_del_keymap('n', '<leader>nr')
+vim.api.nvim_del_keymap("n", "<leader>nr")
 
 local mappings = {
 	-- Custom: Save cursor position before visual selection and restore on <Esc>
@@ -22,28 +22,54 @@ local mappings = {
 			desc = "Hopes Node Jump",
 		},
 	},
-	["n|<leader>nv"] = {
+	
+
+	-- flash
+	["o|r"] = {
 		cmd = "",
 		options = {
 			callback = function()
-				vim.cmd("normal! ma")  -- 跳转到标记 a 的精确位置
+				require("flash").remote()
+			end,
+			noremap = true,
+			silent = true,
+			desc = "Remote Flash",
+		},
+	},
+	["ox|R"] = {
+		cmd = "",
+		options = {
+			callback = function()
+				require("flash").treesitter_search({
+					-- remote treesitter search
+					remote_op = { restore = true, motion = true },
+				})
+			end,
+			noremap = true,
+			silent = true,
+			desc = "Treesitter Search",
+		},
+	},
+	["nxo|s"] = {
+		cmd = "",
+		options = {
+			callback = function()
+				require("flash").jump()
+			end,
+			noremap = true,
+			silent = true,
+			desc = "Flash",
+		},
+	},
+	["nxo|S"] = {
+		cmd = "",
+		options = {
+			callback = function()
 				require("flash").treesitter()
 			end,
 			noremap = true,
 			silent = true,
-			desc = "Flash Treesitter Visual selection",
-		},
-	},
-	["n|<leader>nf"] = {
-		cmd = "",
-		options = {
-			callback = function()
-				vim.cmd("normal! ma")  -- 跳转到标记 a 的精确位置
-				require("flash").treesitter_search()
-			end,
-			noremap = true,
-			silent = true,
-			desc = "Flash Treesitter Find",
+			desc = "Flash Treesitter",
 		},
 	},
 }
