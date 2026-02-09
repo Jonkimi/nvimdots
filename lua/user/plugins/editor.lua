f5407eea79d2e7baa6c2dcc168e2d69251edcece
Jonkimi <JonkimyLiao@gmail.com>
2026-02-09 02:58:14 -0400

    chore: remove print statement in bufdel config

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -5,7 +5,6 @@ editor["ojroques/nvim-bufdel"] = {
 	event = "VeryLazy",
 	-- cmd = { "BufDel", "BufDelAll", "BufDelOthers" },
 	config = function()
-		print("nvim-bufdel config")
 		require("bufdel").setup({
 			next = "tabs",
 			quit = false, -- quit Neovim when last buffer is closed
