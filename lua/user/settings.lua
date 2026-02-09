dd888bfd3eaedc916d2cbef8217b45816803c78a
Jonkimi <JonkimyLiao@gmail.com>
2026-02-09 02:58:27 -0400

    feat(lsp): add rust analyzer and vtsls to block list

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -50,6 +50,7 @@ settings["lsp_deps"] = function(defaults)
 		"volar@3.0.8", -- volar 2.2.12
 		-- "vue_ls@3.0.8", -- volar 3
 		"emmet_language_server",
+		"rust_analyzer",
 	}
 end
 
@@ -63,6 +64,7 @@ settings["null_ls_deps"] = {
 -- 开启 block 后在 formatOnSave 时也会 block
 settings["server_formatting_block_list"] = {
 	html = true,
+	vtsls = true,
 	-- pylsp = true,
 	-- emmet_language_server = true,
 	-- tailwindcss  = true,
