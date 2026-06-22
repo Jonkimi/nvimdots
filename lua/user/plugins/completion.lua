cce25c392e6a87c02e362fc5494f95385a66a818
Jonkimi <JonkimyLiao@gmail.com>
2026-06-22 04:19:18 -0400

    chore(completion): update avante and codeium config

--- lua/user/plugins/completion.lua
+++ lua/user/plugins/completion.lua
@@ -1,10 +1,9 @@
 local completion = {}
--- local use_copilot = require("core.settings").use_copilot
 
 completion["Exafunction/windsurf.nvim"] = {
 	lazy = true,
-	-- event = "VeryLazy",
-	cmd = {"Codeium"},
+	event = "VeryLazy",
+	-- cmd = { "Codeium" },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"hrsh7th/nvim-cmp",
@@ -25,9 +24,9 @@ completion["Exafunction/windsurf.nvim"] = {
 					lua = true,
 					python = true,
 					javascript = true,
-					javascriptreact= true,
+					javascriptreact = true,
 					typescript = true,
-					typescriptreact= true,
+					typescriptreact = true,
 					vue = true,
 					html = true,
 					css = true,
@@ -82,27 +81,159 @@ completion["yetone/avante.nvim"] = {
 			return "make BUILD_FROM_SOURCE=true"
 		end
 	end,
-	lazy = true,
-	-- event = "VeryLazy",
+	-- lazy = true,
+	event = "VeryLazy",
 	cmd = { "AvanteAsk", "AvanteChat" }, -- 改用命令触发
 	version = false, -- Never set this value to "*"! Never!
+	-- config = function(_, opts)
+	-- 	print(vim.inspect(opts)) -- 看看这里 var2 是什么时候变掉的
+	-- end,
+	keys = {
+		{
+			"<leader>aF",
+			function()
+				local avante = require("avante")
+				local sidebar = require("avante.sidebar")
+				avante:open_sidebar()
+				avante.get().file_selector:open()
+			end,
+			desc = "Select file",
+		},
+		{
+			"<leader>a+",
+			function()
+				local tree_ext = require("avante.extensions.nvim_tree")
+				tree_ext.add_file()
+			end,
+			desc = "Select file in NvimTree",
+			ft = "NvimTree",
+		},
+		{
+			"<leader>a-",
+			function()
+				local tree_ext = require("avante.extensions.nvim_tree")
+				tree_ext.remove_file()
+			end,
+			desc = "Deselect file in NvimTree",
+			ft = "NvimTree",
+		},
+	},
 	---@module 'avante'
 	---@type avante.Config
 	opts = {
 		-- add any opts here
 		-- for example
-		provider = "copilot",
+		-- mode = "legacy",
+		-- provider = "copilot",
+		provider = "gemini",
+		-- provider = "claude-code",
 		-- provider = "openai",
+		disabled_tools = {"rag_search", "web_search"},
 		providers = {
-			-- openai = {
-			-- 	endpoint = "https://bh.jonkimi.com/v1",
-			-- 	model = "gpt-4.1"
-			-- }
+			openai = {
+				endpoint = "https://bh.jonkimi.com/v1",
+				model = "gpt-4.1",
+			},
+			["openai-gpt-4o-mini"] = {
+				hide_in_model_selector = true,
+			},
+			-- gemini = {
+			-- 	endpoint = "https://gemini-proxy.jonkimi.com/v1beta/models",
+			-- 	model = "gemini-2.0-flash",
+			-- 	temperature = 0.3,
+			-- },
+			copilot = {
+				hide_in_model_selector = true, -- 隐藏但不完全禁用
+			},
+			claude_code = {
+				endpoint = "https://foxgpt.jonkimi.com/api/anthropic",
+				-- endpoint = "http://127.0.0.1:3000/api/anthropic",
+				-- endpoint = "http://127.0.0.1:8000",
+				api_key_name = "ANTHROPIC_FOX_API_KEY",
+				model = "claude-sonnet-4-5-20250929",
+				extra_request_body = {
+					temperature = 0.3,
+				},
+			},
+			["claude_code-t"] = {
+				__inherited_from = "claude_code",
+				-- api_key_name = "ANTHROPIC_FOX_API_KEY",
+				model = "claude-sonnet-4-5-20250929",
+				extra_request_body = {
+					temperature = 1.0,
+					thinking = { type = "enabled", budget_tokens = 4000 },
+				},
+			},
+			["claude_code-opus"] = {
+				__inherited_from = "claude_code",
+				-- api_key_name = "ANTHROPIC_FOX_API_KEY",
+				-- model = "claude-opus-4-5-20251101",
+				model = "claude-opus-4-6",
+				extra_request_body = {
+					temperature = 1.0,
+					thinking = { type = "enabled", budget_tokens = 31999},
+				},
+			},
+			["claude_code-haiku"] = {
+				__inherited_from = "claude_code",
+				-- api_key_name = "ANTHROPIC_FOX_API_KEY",
+				model = "claude-haiku-4-5-20251001",
+				extra_request_body = {
+					temperature = 1.0,
+					thinking = { type = "enabled", budget_tokens = 4000 },
+				},
+			},
+			claude = {
+				hide_in_model_selector = true,
+			},
 			gemini = {
-				endpoint = "https://gemini-proxy.jonkimi.com/v1beta/models",
-				model = "gemini-2.0-flash",
-				temperature = 0.3,
-			}
+				endpoint = "https://geminicli-api-opt.jonkimi.com/v1beta/models",
+				model = "gemini-2.5-pro",
+				temperature = 0.6,
+				api_key_name = "GEMINI_API_KEY",
+			},
+			gemini_3_flash = {
+				__inherited_from = "gemini",
+				-- endpoint = "https://geminicli-api-opt.jonkimi.com/v1beta/models",
+				model = "gemini-3-flash-preview",
+				temperature = 0.6,
+				-- api_key_name = "GEMINI_API_KEY",
+			},
+			gemini_3_pro = {
+				__inherited_from = "gemini",
+				-- endpoint = "https://geminicli-api-opt.jonkimi.com/v1beta/models",
+				model = "gemini-3-pro-preview",
+				temperature = 0.6,
+				-- api_key_name = "GEMINI_API_KEY",
+			},
+			vertex_claude = {
+				hide_in_model_selector = true,
+			},
+			vertex = {
+				hide_in_model_selector = true,
+			},
+		},
+		acp_providers = {
+			["claude-code"] = {
+				command = "npx",
+				args = { "@zed-industries/claude-code-acp" },
+				env = {
+					NODE_NO_WARNINGS = "1",
+					-- ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY"),
+					ANTHROPIC_AUTH_TOKEN = os.getenv("ANTHROPIC_AUTH_TOKEN"),
+					ANTHROPIC_BASE_URL = os.getenv("ANTHROPIC_BASE_URL"),
+				},
+			},
+		},
+		windows = {
+			width = 30,
+			input = {
+				prefix = "> ",
+				height = 12, -- 输入框高度（行数）
+			},
+			selected_files = {
+				height = 6, -- 选中文件窗口的最大高度
+			},
 		},
 	},
 	dependencies = {
@@ -114,7 +245,7 @@ completion["yetone/avante.nvim"] = {
 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
 		"stevearc/dressing.nvim", -- for input provider dressing
-		"folke/snacks.nvim", -- for input provider snacks
+		-- "folke/snacks.nvim", -- for input provider snacks
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
@@ -142,6 +273,29 @@ completion["yetone/avante.nvim"] = {
 			},
 			ft = { "markdown", "Avante" },
 		},
+	},
+}
+
+completion["folke/snacks.nvim"] = {
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		-- bigfile = { enabled = true },
+		-- dashboard = { enabled = true },
+		-- explorer = { enabled = true },
+		-- indent = { enabled = true },
+		-- input = { enabled = true },
+		-- picker = { enabled = true },
+		-- notifier = { enabled = true },
+		-- quickfile = { enabled = true },
+		-- scope = { enabled = true },
+		-- scroll = { enabled = true },
+		-- statuscolumn = { enabled = true },
+		-- words = { enabled = true },
 	},
 }
 
